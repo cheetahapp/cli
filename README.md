620182ac70a0a26dfa69a598fcbefc9c88998b8a
Moin <me@moin.im>
2018-11-24 15:50:45 +0500

    first 🦄

---
+++ README.md
@@ -0,0 +1,26 @@
+## Cheetah CLI
+
+Use command line to download a file with multiple parallel connections. It uses [Cheetah Downloader](https://github.com/cheetahapp/downloader) behind the scene.
+
+
+### Install
+
+Yarn or NPM, you know the drill.
+
+```bash
+npm i cheetah-cli -g
+```
+
+### Usage
+
+```bash
+cheetah [targetDirectory]
+```
+
+It'll ask for the URL to download, just paste and enter.
+
+> targetDirectory will be `~/Downloads` by default.
+
+### License
+
+[MIT](./LICENSE). Copyright (c) [Moin Uddin](https://moin.im)
---
+++ index.js
@@ -0,0 +1,70 @@
+#!/usr/bin/env node
+
+'use strict'
+
+const fs = require('fs')
+const os = require('os')
+const path = require('path')
+
+const prompt = require('prompt')
+const _progress = require('cli-progress')
+const download = require('cheetah-downloader')
+
+const bar1 = new _progress.Bar({
+  format: '[{bar}] {percentage}% | ETA: {remain} | Speed: {speed} kb/s'
+}, _progress.Presets.shades_grey)
+
+function milliToX (millis) {
+  var hours = Math.floor(millis / 36e5)
+  var mins = Math.floor((millis % 36e5) / 6e4)
+  var secs = Math.floor((millis % 6e4) / 1000)
+  return hours + ':' + mins + ':' + secs
+}
+
+const targetDir = process.argv[2] || path.join(os.homedir(), '/Downloads')
+prompt.message = '>'
+prompt.start()
+
+try {
+  fs.accessSync(targetDir, fs.W_OK)
+} catch (e) {
+  console.log('Target folder not accessible:', e.code, targetDir)
+  return
+}
+
+prompt.get({
+  properties: {
+    url: {
+      type: 'string',
+      required: true,
+      message: 'Please enter a valid URL'
+    }
+  }
+}, function(err, res) {
+  try {
+    const cheetah = download(res.url, targetDir, {
+      tempDir: __dirname + '/temp/'
+    })
+
+    cheetah
+      .on('progress', function (progress) {
+        var remain = milliToX(progress.timeRemaining * 1000)
+        bar1.update(progress.percent, {
+          remain: remain,
+          speed: progress.speed
+        })
+      })
+      .on('complete', function(res) {
+        bar1.stop()
+        console.log('Downloaded at:\n', res.path)
+      })
+      .start()
+
+    bar1.start(100, 0, {
+      remain: '0',
+      speed: '0'
+    })
+  } catch (e) {
+    console.log('Some error occurred', e)
+  }
+})
